fde1b9d85037c06d77bb496c83c4d9b32091c26a
amwau <stacykate55@gmail.com>
2022-10-14 09:53:58 +0300

    about me section

---
+++ week 2/bootstrap-5.2.2-dist/index.js
@@ -0,0 +1,20 @@
+let numOne = 1;
+let numTwo = 2;
+let numThree = 5,8;
+numOne.toString();
+
+//strings
+let firstString = 'this is a test string';
+let secondString = 'This is a second test string';
+
+//booleans
+let truthValue = True;
+let falseValue =false;
+//arays
+let firstArray = [firstString, secondString];
+let secondArray = [numOne, firstString, truthValue, firstArray]
+
+//functions
+//Anatomy ->const FunctionName = () =>{}
+
+const addTwoNumbers = (num1, num2) =>{return num1 + num2}
